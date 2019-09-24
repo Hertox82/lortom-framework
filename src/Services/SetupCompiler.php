611c6fd6da05d6040fab70d284678a3ed99a0f95
Hernan Ariel De Luca <hadeluca@gmail.com>
2019-09-24 09:58:16 +0200

    modify the setup compiler

--- src/Services/SetupCompiler.php
+++ src/Services/SetupCompiler.php
@@ -14,11 +14,11 @@ use File;
 class SetupCompiler {
 
     protected $plugins = [
-        "Hardel@Dashboard" => "1.1.0",
-        "Hardel@Settings" => "1.1.0",
-        "Hardel@Plugin" => "1.1.0",
-        "Hardel@Website" => "1.1.0",
-        "Hardel@File" => "1.1.0"
+        "Hardel@Dashboard" => "1.2.0",
+        "Hardel@Settings" => "1.2.0",
+        "Hardel@Plugin" => "1.2.0",
+        "Hardel@Website" => "1.2.0",
+        "Hardel@File" => "1.2.0"
     ];
 
     protected $node;
